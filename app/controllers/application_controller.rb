94386b02df3a6c8ecdcabb1de5acaea71d64f509
Daniel Chin <DanChin327@gmail.com>
2024-04-16 18:04:09 +0900

    debugging logout error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :user_signed_in?
+
+  private
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def user_signed_in?
+    !!current_user
+  end
 end
